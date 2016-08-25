3e5de1acbd9e7fd3c780beba8724a118db82031d
Daniel Tse <tsetsefly@gmail.com>
2016-08-24 23:51:40 -0700

    d02x06: files

---
+++ d02/ex06/ft_putnbr.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		ft_putchar('-');
+		num *= -1;
+	}
+	if (num > 9)
+		ft_putnbr(num / 10);
+	ft_putchar(num % 10 + 48);
+}
---
+++ d02/ex06/main.c
@@ -0,0 +1,12 @@
+ void ft_putchar(char c);
+ void ft_putnbr(int nb);
+
+ int main ()
+ {
+ 	int num;
+
+ 	num = 123;
+ 	ft_putnbr(num);
+ 	ft_putchar('\n');
+ 	return 0;
+ }

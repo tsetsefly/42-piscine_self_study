0662490deb8ca716366cfb06e9ddf3f079e6fdc8
Daniel Tse <tsetsefly@gmail.com>
2016-08-27 16:58:06 -0700

    d03x07: final submission

---
+++ d03/ex07/ft_strrev.c
@@ -0,0 +1,73 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		num *= -1;
+		ft_putchar('-');
+	}
+	if (num > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return(i);
+}
+
+char *ft_strrev(char *str)
+{
+	int i;
+	int j;
+	char tmp;
+
+	i = 0;
+	j = ft_strlen(str) - 1;
+	while (i < j)
+	{
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+		i++;
+		j--;
+	}
+	return (str);
+}
+
+int main ()
+{
+	char str[] = "abcde"; //string needs to be dynamic to work!
+	char *rev;
+
+	rev = ft_strrev(str);
+	ft_putstr(rev);
+	ft_putchar('\n');
+	return 0;
+}

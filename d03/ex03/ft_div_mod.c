70ef1a351341f4c151fe6fc85d61492c66504da9
Daniel Tse <tsetsefly@gmail.com>
2016-08-26 17:38:48 -0700

    d03x03: final files

---
+++ d03/ex03/ft_div_mod.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		num *= -1;
+		ft_putchar('-');
+	}
+	if (num > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int main ()
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 100;
+	b = 7;
+	ft_div_mod(a, b, &div, &mod);
+	ft_putstr("a = ");
+	ft_putnbr(a);
+	ft_putstr(", b = ");
+	ft_putnbr(b);
+	ft_putchar('\n');
+	ft_putstr("div = ");
+	ft_putnbr(div);
+	ft_putstr(", mod = ");
+	ft_putnbr(mod);
+	ft_putchar('\n');
+	return 0;
+}

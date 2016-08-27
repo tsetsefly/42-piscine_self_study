ba11403b5e890fb7ebf01920bf16caa40b61c84e
Daniel Tse <tsetsefly@gmail.com>
2016-08-26 17:32:08 -0700

    d03x02: final files

---
+++ d03/ex02/ft_swap.c
@@ -0,0 +1,63 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		num *= -1;
+		ft_putchar('-');
+	}
+	if (num > 9)
+		ft_putnbr(nb / 10);
+	ft_putchar(nb % 10 + 48);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void ft_swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int main ()
+{
+	int a;
+	int b;
+	
+	a = 2;
+	b = 4;
+	ft_putstr("a1 = ");
+	ft_putnbr(a);
+	ft_putstr(", b1 = ");
+	ft_putnbr(b);
+	ft_putchar('\n');
+	ft_swap(&a, &b);
+	ft_putstr("a2 = ");
+	ft_putnbr(a);
+	ft_putstr(", b2 = ");
+	ft_putnbr(b);
+	ft_putchar('\n');
+	return 0;
+}

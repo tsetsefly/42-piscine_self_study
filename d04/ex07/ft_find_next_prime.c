5781eaa6dd87b99f962f894e248483c8cd19956f
Daniel Tse <tsetsefly@gmail.com>
2016-08-30 23:40:48 -0700

    d04x07: final files

---
+++ d04/ex07/ft_find_next_prime.c
@@ -0,0 +1,81 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		num *= -1;
+		ft_putchar('-');
+	}
+	if (num > 9)
+		ft_putnbr(num / 10);
+	ft_putchar(num % 10 + 48);
+	return (num);
+}
+
+int ft_is_prime(int nb)
+{
+	int div;
+
+	div = nb / 2;
+	if (nb < 3)
+		return (0);
+	else
+	{
+		while (div > 1)
+		{
+			if (nb % div == 0)
+				return (0);
+			div--;
+		}
+	}
+	return (1);
+}
+
+int ft_find_next_prime(int nb)
+{
+	int div;
+
+	div = nb;
+	if (nb < 3)
+		return (2);
+	else
+	{
+		while (1)
+		{	
+			if (ft_is_prime(div))
+				return (div);
+			else
+				div++;
+		}
+	}
+}
+
+int main ()
+{
+	int result;
+
+	result = ft_find_next_prime(1000);
+	ft_putnbr(result);
+	return 0;
+}

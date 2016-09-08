1709522ac080708fb9de2efc129122624c616a03
Daniel Tse <tsetsefly@gmail.com>
2016-09-08 12:09:32 -0700

    d05x05: have it working for correct cases

--- d05/ex05/ft_strstr.c
+++ d05/ex05/ft_strstr.c
@@ -31,8 +31,27 @@ int ft_strlen(char *str)
 char *ft_strstr(char *str, char *to_find)
 {
 	int i;
+	int j;
+	int k;
 
-
+	i = 0;
+	while (i < ft_strlen(str) - ft_strlen(to_find))
+	{
+		if (str[i] == to_find[0])
+		{
+			k = i;
+			j = 0;
+			while (str[k] == to_find[j])
+			{
+				j++;
+				k++;
+			}
+			if (j == ft_strlen(to_find))
+				return (&str[i]);
+		}
+		i++;
+	}
+	return (0);
 }
 
 int main ()
@@ -41,11 +60,11 @@ int main ()
 	char *to_find;
 
 	str = "    the quick brown fox jumps over the lazy dog... ";
-	to_find = " fox";
+	to_find = "xf";
 	ft_putstr("ft_strstr = ");
 	ft_putstr(ft_strstr(str, to_find));
 	ft_putchar('\n');
-	ft_putstr("strstr = ");
+	ft_putstr("   strstr = ");
 	ft_putstr(strstr(str, to_find));
 	ft_putchar('\n');
 	return 0;	

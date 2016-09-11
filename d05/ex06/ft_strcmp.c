86110be69306f893ff4913ced3d83b8ac00b863a
Daniel Tse <tsetsefly@gmail.com>
2016-09-10 17:44:00 -0700

    d05x06: final files

--- d05/ex06/ft_strcmp.c
+++ d05/ex06/ft_strcmp.c
@@ -43,25 +43,32 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-// int ft_strcmp(char *s1, char *s2)
-// {
-// 	int i;
+int ft_strcmp(char *s1, char *s2)
+{
+	int i;
 
-	
-
-// 	return ();
-// }
+	i = 0;
+	while(s1[i] || s2[i])
+	{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
 
 int main ()
 {
 	char *s1;
 	char *s2;
 
-	s1 = "banana";
-	s2 = "banana";
-	// ft_putstr("ft_strcmp = ");
-	// ft_putnbr(ft_strcmp(s1, s2));
-	// ft_putchar('\n');
+	s1 = "";
+	s2 = "bananaaa";
+	ft_putstr("ft_strcmp = ");
+	ft_putnbr(ft_strcmp(s1, s2));
+	ft_putchar('\n');
 	ft_putstr("   strcmp = ");
 	ft_putnbr(strcmp(s1, s2));
 	ft_putchar('\n');

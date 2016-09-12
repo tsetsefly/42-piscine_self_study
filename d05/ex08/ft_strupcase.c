efd4b2753e1b187895c7b55bfd6d63c2b2b14452
Daniel Tse <tsetsefly@gmail.com>
2016-09-12 15:54:21 -0700

    d05x08: final files, still need to understand what is going on with dynamic variables

--- d05/ex08/ft_strupcase.c
+++ d05/ex08/ft_strupcase.c
@@ -17,16 +17,49 @@ void ft_putstr(char *str)
 	}
 }
 
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 char *ft_strupcase(char *str)
 {
-	
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 'a' - 'A';
+		i++;
+	}
+	return (str);
 }
 
 int main ()
 {
-	char *str;
+	char str[] = "aBCDeFGHi";
 
-	str = "aBCDeFGHi";
 	ft_putstr(ft_strupcase(str));
 	return (0);
 }

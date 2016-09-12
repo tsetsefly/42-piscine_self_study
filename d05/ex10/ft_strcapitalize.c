a3e22f1e0fbed57a52c8234b6d86a2783079bd84
Daniel Tse <tsetsefly@gmail.com>
2016-09-12 16:45:58 -0700

    d05x10: final files

--- d05/ex10/ft_strcapitalize.c
+++ d05/ex10/ft_strcapitalize.c
@@ -17,3 +17,38 @@ void ft_putstr(char *str)
 	}
 }
 
+char *ft_strcapitalize(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
+			i++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 'a' - 'A';
+			i++;
+		}
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{	
+			if (str[i] >= 'A' && str[i] <= 'A')
+			{
+				str[i] += 'a' - 'A';
+				i++;
+			}
+			else
+				i++;
+		}
+	}
+	return (str);
+}
+
+int main ()
+{
+	char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	ft_putstr(ft_strcapitalize(str));
+	return (0);
+}

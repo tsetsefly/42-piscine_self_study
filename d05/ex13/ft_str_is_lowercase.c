a92bbab1e8b56f6c51034683567a8261915807a5
Daniel Tse <tsetsefly@gmail.com>
2016-09-12 17:28:43 -0700

    d05x13: deleted unnessary file

---
+++ d05/ex13/ft_str_is_lowercase.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void ft_putnbr(int nb)
+{
+	long num;
+
+	num = nb;
+	if (num < 0)
+	{
+		num *= -1;
+		ft_putchar('-');
+	}
+	if (num > 9)
+		ft_putnbr(num / 10);
+	ft_putchar(num % 10 + '0');
+}
+
+int ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int main ()
+{
+	int result;
+	char *str;
+
+	str = "Aa";
+	result = ft_str_is_lowercase(str);
+	ft_putnbr(result);
+	return (0);
+}

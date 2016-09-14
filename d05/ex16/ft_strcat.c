5b6315a5a2d175784e6e97db529b232bac601daf
Daniel Tse <tsetsefly@gmail.com>
2016-09-13 23:42:26 -0700

    d05x16: initial files

---
+++ d05/ex16/ft_strcat.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+char *ft_strcat(char *dest, char *src)
+{
+
+
+}
+
+int main ()
+{
+
+	return (0);
+}

0afdf182918886f59d8747c4c3e32e9d3df09212
Daniel Tse <tsetsefly@gmail.com>
2016-09-14 12:00:58 -0700

    d05x17: initial files

---
+++ d05/ex17/ft_strncat.c
@@ -0,0 +1,55 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strncat(char *dest, char *src, int nb)
+{
+	int i;
+	int dest_len;
+
+	dest_len = ft_strlen(dest);
+	i = 0;
+	while (src[i])
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
+
+int main ()
+{
+	char dest[] = "abcdefg";
+	char src[] = "hijklmnop";
+
+	ft_putstr(ft_strncat(dest, src, 16));
+	ft_putchar('\n');
+	return (0);
+}
+

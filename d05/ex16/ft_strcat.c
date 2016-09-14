bfef9cee022ce745fc551ca43234810da8d1276c
Daniel Tse <tsetsefly@gmail.com>
2016-09-14 11:56:57 -0700

    d05x16: final files

--- d05/ex16/ft_strcat.c
+++ d05/ex16/ft_strcat.c
@@ -17,14 +17,38 @@ void ft_putstr(char *str)
 	}
 }
 
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
 char *ft_strcat(char *dest, char *src)
 {
+	int i;
+	int dest_len;
 
-
+	dest_len = ft_strlen(dest);
+	i = 0;
+	while (src[i])
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
 
 int main ()
 {
+	char dest[] = "abcdefg";
+	char src[] = "hijklmnop";
 
+	ft_putstr(ft_strcat(dest, src));
+	ft_putchar('\n');
 	return (0);
 }
